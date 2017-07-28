be6ab46d9768dcc6b36c88be7dc2258e4354337d
Yves Riedener <git@reecube.com>
2017-07-28 16:11:14 +0200

    improved some scripts

--- windows-update.bat
+++
@@ -1,6 +0,0 @@
-composer install
-
-php bin/console assets:install
-php bin/console assetic:dump
-
-taskkill /F /IM php-cgi.exe /T
--- windows-update.sh
+++ windows-update.sh
@@ -3,6 +3,5 @@
 composer install
 
 php bin/console assets:install
-php bin/console assetic:dump
 
-taskkill /F /IM php-cgi.exe /T
+taskkill -F -IM php-cgi.exe -T
